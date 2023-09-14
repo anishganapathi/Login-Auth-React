cb3f19d41158c99452931cd4926a3c1f3e6f5ca1
akash <anishganapathi19@gmail.com>
2023-09-14 22:53:29 +0530

    Updated login details
    + Added redirect routes from login to profile
    + Added credentials to log-in
    + Added Shit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
-import {index} from '../index.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = () => {
+   
+    if (email === 'admin' && password === '123') {
+     
+      navigate('/profile');
+    } else {
+     
+      setError('Invalid email or password');
+    }
+  };
+
   return (
     <section className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
       <div className="md:w-1/3 max-w-sm">
@@ -17,12 +36,17 @@ const Login = () => {
           className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
           type="text"
           placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
           type="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+        {error && <div className="text-red-500 mt-2">{error}</div>}
         <div className="mt-4 flex justify-between font-semibold text-sm">
           <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
             <input className="mr-1" type="checkbox" />
@@ -34,11 +58,21 @@ const Login = () => {
           >
             Forgot Password?
           </a>
+          
+          
         </div>
+        <br />
+        <div class="flex justify-center">
+  <a class="text-600">
+    ****email: admin, password: 123****
+  </a>
+</div>
+
         <div className="text-center md:text-left">
           <button
+            onClick={handleLogin}
             className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
-            type="submit"
+            type="button"
           >
             Login
           </button>
