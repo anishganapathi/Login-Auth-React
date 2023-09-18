6df9c83a430e851a775129340f3ac33c8ac7b431
akash <anishganapathi19@gmail.com>
2023-09-18 19:48:06 +0530

    Changes in Home.jsx
    + Added divs in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import Spline from '@splinetool/react-spline';
 
 function Home() {
   return (
-    <Spline scene="https://prod.spline.design/Q9x2SIWgAX00vbyw/scene.splinecode" />
+    <>
+    <div>
+     <Spline scene="https://prod.spline.design/Q9x2SIWgAX00vbyw/scene.splinecode" />
+    </div>
+    </>
+    
   );
 }
 
