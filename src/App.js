5782f9aae2181663bef1ba0eb977ddf714b0936c
akash <anishganapathi19@gmail.com>
2023-09-25 23:24:16 +0530

    + Created Pen.jsx
    + Added text animations in pen.jsx
    + Added required css for pen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from '../src/components/Login';
 import Profile from "./pages/profile";
 import Home from "./components/Home";
+import Pen from "./components/Pen";
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -16,7 +17,8 @@ function App() {
         <Routes>
           <Route path="/about" element={<Profile />} />
           <Route exact path="/" element={<Login/>}/>
-          {/* <Route path="/home" element={<Home />} /> */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/pen" element={<Pen />} />
         </Routes>
       
     </Router>
