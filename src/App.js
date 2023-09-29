4098992030fe31452edda48811a80eb20ddbb25b
akash <anishganapathi19@gmail.com>
2023-09-29 21:06:01 +0530

    - Removed Pen.jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Login from '../src/components/Login';
 import Profile from "./pages/profile";
 import Home from "./components/Home";
-import Pen from "./components/Pen";
 
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -18,7 +17,6 @@ function App() {
           <Route path="/about" element={<Profile />} />
           <Route exact path="/" element={<Login/>}/>
           <Route path="/home" element={<Home />} />
-          <Route path="/pen" element={<Pen />} />
         </Routes>
       
     </Router>
