d4d189c1f6a7a6a8fc25172599eb782aecea7c3d
Anish <anishganapathi19@gmail.com>
2023-11-18 11:51:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import Login from '../src/components/Login';
+import React from "react";
+import Login from "../src/components/Login";
 import Profile from "./pages/profile";
 import Home from "./components/Home";
 
-
-
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-  
       <Router>
-    
-        <Routes>
+        {/* <Routes>
           <Route path="/about" element={<Profile />} />
           <Route exact path="/" element={<Login/>}/>
           <Route exact path="/profile" element={<Profile/>}/>
           <Route exact path="/home" element={<Home/>}/>
-        </Routes>
-      
-    </Router>
+        </Routes> */}
+      </Router>
     </div>
   );
 }
