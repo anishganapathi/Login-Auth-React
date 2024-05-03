1c30e42d2cd8094f562640a3b3fe9020fd159a69
Anish <104456169+anishganapathi@users.noreply.github.com>
2024-05-03 11:58:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        {/* <Routes>
+        <Routes>
           <Route path="/about" element={<Profile />} />
           <Route exact path="/" element={<Login />} />
           <Route exact path="/profile" element={<Profile />} />
           <Route exact path="/home" element={<Home />} />
-        </Routes> */}
+        </Routes> 
       </Router>
     </div>
   );
